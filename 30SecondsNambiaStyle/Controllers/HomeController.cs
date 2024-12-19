b47510800891ee9bc307ecfb9281eb6c8634663c
Romario.Arnat <Romario.Arnat@GES140-RA>
2024-12-19 22:24:16 +0200

    added more words

--- 30SecondsNambiaStyle/Controllers/HomeController.cs
+++ 30SecondsNambiaStyle/Controllers/HomeController.cs
@@ -20,37 +20,47 @@ namespace _30SecondsNambiaStyle.Controllers
 
         public IActionResult GetNamibianCards()
         {
-            // Namibian-themed words
-            var words = new List<string>
-        {
-            "Fishrot", "Windhoek Lager", "Tafel Lager", "Kapana", "Fat Cakes",
-            "The Dogg", "King TeeDee", "Sam Nujoma", "Namib Desert", "Etosha",
-            "Fish River Canyon", "Skeleton Coast", "Spitzkoppe", "Quiver Tree", "Oshakati",
-            "Herero Dress", "Himba Women", "Bushmen", "NAMAs", "Ricardo Mannetti",
-            "Brave Warriors", "Independence Day", "March 21", "Mealie Pap", "Matangara",
-            "Oshikundu", "Biltong", "Droëwors", "Meatco", "Bank Windhoek",
-            "German Colonial Rule", "Caprivi Strip", "UNAM", "NUST", "Okahandja",
-            "Gobabis", "Namib Wild Horses", "Oryx", "Springbok", "Dune 45",
-            "Deadvlei", "Epupa Falls", "Kolmanskop", "Otjikoto Lake", "Hoba Meteorite",
-            "Sam Shivute", "Air Namibia", "Ongwediva Trade Fair", "Hage Geingob", "Land of the Brave",
-            "NBC", "Namibian Sun", "Road Fund Administration", "TransNamib", "Sunburn",
-            "Sandwich Harbour", "Desert Adapted Elephants", "Windhoek Showgrounds", "Okavango Delta",
-            "Gondwana Lodges", "Safari", "NamibRand Nature Reserve", "Katutura", "Namport",
-            "Old Mutual", "NAMDEB", "Rundu", "Warm Land", "African Time","Chula Chula",
-            "Friendly People", "Fat Cakes Vendor", "Kapana Corner", "Namibian Dollar", "Okavango River", "African Stars",
-        };
-
-            // Shuffle words and group into lists of 5
-            var random = new Random();
-            var shuffledWords = words.OrderBy(x => random.Next()).ToList();
-            var cards = new List<List<string>>();
-
-            for (int i = 0; i < shuffledWords.Count; i += 5)
+            try
             {
-                cards.Add(shuffledWords.Skip(i).Take(5).ToList());
-            }
 
-            return Json(cards); // Return the randomized cards as JSON
+                // Namibian-themed words
+                var words = new List<string>
+                   {
+                        "Fishrot", "Windhoek Lager", "Tafel Lager", "Kapana", "Fat Cakes","Ohole",
+                         "The Dogg", "King TeeDee", "Sam Nujoma", "Namib Desert", "Etosha",
+                        "Fish River Canyon", "Skeleton Coast", "Spitzkoppe", "Quiver Tree", "Oshakati",
+                         "Herero Dress", "Himba Women", "Bushmen", "NAMAs", "Ricardo Mannetti",
+                        "Brave Warriors", "Independence Day", "March 21", "Mealie Pap", "Matangara","Donkey Festival",
+                         "Oshikundu", "Biltong", "Droëwors", "Meatco", "Bank Windhoek",
+                         "German Colonial Rule", "Caprivi Strip", "UNAM", "NUST", "Okahandja","Swapo","Nxa","Sharp", "Aweh",
+                         "Gobabis", "Namib Wild Horses", "Oryx", "Springbok", "Dune 45","Taxi",
+                        "Deadvlei", "Epupa Falls", "Kolmanskop", "Otjikoto Lake", "Hoba Meteorite",
+                         "Sam Shivute", "Air Namibia", "Ongwediva Trade Fair", "Hage Geingob", "Land of the Brave",
+                        "NBC", "Namibian Sun", "Road Fund Administration", "TransNamib", "Sunburn",
+                     "Sandwich Harbour", "Desert Adapted Elephants", "Windhoek Showgrounds", "Okavango Delta",
+                      "Gondwana Lodges", "Safari", "NamibRand Nature Reserve", "Katutura", "Namport",
+                    "Old Mutual", "NAMDEB", "Rundu", "Warm Land", "African Time","Chula Chula","Mahangu", "Oshiwambo", "Damara Punch",
+                     "Friendly People", "Fat Cakes Vendor", "Sacky Shanghala", "Namibian Dollar", "Okavango River", "African Stars","Mjolo", "Ke December","Samuel Nujoma","Swakop"
+                    };
+
+
+                // Shuffle words and group into lists of 5
+                var random = new Random();
+                var shuffledWords = words.OrderBy(x => random.Next()).ToList();
+                var cards = new List<List<string>>();
+
+                for (int i = 0; i < shuffledWords.Count; i += 5)
+                {
+                    cards.Add(shuffledWords.Skip(i).Take(5).ToList());
+                }
+
+                return Json(cards); // Return the randomized cards as JSON
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         public IActionResult Privacy()
